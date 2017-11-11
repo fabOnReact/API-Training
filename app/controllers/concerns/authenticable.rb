844b1c318370da3f271dbd642c330e9e1d15a1d9
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-11-11 18:29:40 +0100

    Adds user_signed_in? method to know whether the user is logged in or not

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -2,4 +2,8 @@ module Authenticable
 	def current_user
 		@user ||= User.find_by(auth_token: request.headers['Authorization'])
 	end
+
+	def authenticate_with_token!
+		render json: { errors: 'Not Authenticated' }, status: :unauthorized unless current_user.present?
+	end
 end
--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -19,4 +19,20 @@ describe Authenticable do
 			expect(authentication.current_user.auth_token).to eql @user.auth_token
 		end
 	end
+
+	describe 'authenticate with token' do
+		before(:each) do
+			@user = FactoryGirl.create(:user)
+			authentication.stub(:current_user).and_return(nil)
+			response.stub(:body).and_return({'errors' => 'Not Authenticated'}.to_json)
+			response.stub(:response_code).and_return(401)
+			authentication.stub(:response).and_return(response)
+		end
+
+		it 'should return an error messages' do
+			expect(json_response[:errors]).to eql('Not Authenticated')
+		end	
+
+		it { should respond_with 401}
+	end
 end
