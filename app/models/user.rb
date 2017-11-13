964e1dc540bb8dda278965c79a2475dadc3b3ac6
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-11-13 18:14:24 +0100

    Before updating Rails to 4.1, because ActiveRecord is having issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :auth_token, uniqueness: true
+  has_many :products, dependent: :destroy
 
   def generate_authentication_token!
   	begin
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,15 @@ FactoryGirl.define do
 	email { FFaker::Internet.email }
 	password '12345678'
 	password_confirmation '12345678'    
+
+	factory :user_with_products do 
+		transient do 
+			products_count 5
+		end
+
+		after(:create) do |user, evaluator|
+			create_list(:product, evaluator.products_count, user: user)
+		end
+	end
   end
 end
